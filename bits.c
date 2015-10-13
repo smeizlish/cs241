a28ac7cea489d6259e46e91a068eac0972c80cd2
Sam Meizlish <smeizlis@clyde>
2015-10-13 17:08:07 -0400

    fixed decode_bits function

--- bits.c
+++ bits.c
@@ -12,27 +12,24 @@ void decode_bits (int ch)
 {
     static int buff[ CHAR_BIT ];
     static int count = 0;
+
     if ( ch == '0' | ch == '1' )
     {
-	buff[count] = ch;
+	buff[CHAR_BIT-1-count] = ch - '0';
 	count++;
 	if ( count == CHAR_BIT )
 	{
-	    //convert binary in buff to decimal and print it
 	    int n = 0;	
-	    int dec = 0;
- 	    while (count >= 0){
-		if (buff[count] == 1){
-			dec += 1<<n;
-			count--;
-			n++;
-			printf('%i \n', dec);
-			putchar(dec);
-	    		count = 0;
-	}		    
-    }
-}
-}
+	    int dec = CHAR_BIT-1;
+ 	    while (dec >= 0)
+	    {
+		n += ( buff[dec] << dec );
+		dec--;
+	    }
+	    putchar( n );
+	    count = 0;
+	}
+    }   
     else if ( isspace(ch) != 0 )
     {
 	return;
