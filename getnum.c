b58687c47f0d9a281f3aa24071060b7003706604
zackalak <zsheldon@oberlin.edu>
2015-10-13 18:15:11 -0400

    Update to getnum.c
    Has not been tested yet.

--- getnum.c
+++ getnum.c
@@ -6,28 +6,62 @@ char neg;
 
 
 long getnum(void) {
-	long output;
-	char input[] = malloc(sizeof(long)+2+1)
+	long output = 0;
 
-	input = scanf();
-
-
-	while((ch=getchar()) != (EOF|' ')) {//avoid whitespace
-		if ('-' == ch) {
+	ch = getchar();
+	while((ch) != (EOF)) {//avoid whitespace
+		if (isspace(ch)){
+		break
+}
+		else if ('-' == ch) {
 			neg = 1;
 			ch = getchar();
 }
-		if ('0' == ch) {
+		else if ('0' == ch) {
 			ch = getchar();
-			switch (ch) {
-			case 'b'://number is binary
-		
-			case 'x'://number is hex
-			case '1': case '2': case '3': case '4': case '5': 
-			case '6': case '7':
-				//do octal stuff
-		else {
-	//do decimal stuff
-			
 
+			switch (ch) {
+
+			case 'b'://number is binary
+				ch = getchar();
+				while (ch == '1'|'0') {
+					output = output*2 + ch;
+					ch = getchar();
 }
+				
+				if (ch != '1'|'0') {
+					printf("%s", "ERROR");
+					break;
+				return output;
+}
+			case 'x'://number is hex
+				ch = getchar();
+				while (ch == '1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'|'0'|'a'|'b'|'c'|'d'|'e'|'f') {
+					output = output*16 + ch;\
+					ch = getchar();
+}
+				
+				if (ch != '1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'|'0'|'a'|'b'|'c'|'d'|'e'|'f') {
+					break;
+}
+				return output;
+			default:
+				ch = getchar();
+				if (ch != '1'|'2'|'3'|'4'|'5'|'6'|'7'){
+					break;//also print error
+				}
+
+				while (ch == '1'|'2'|'3'|'4'|'5'|'6'|'7'){
+					output = output*8 + ch;
+					ch = getchar();
+				}
+				return output;
+				
+}//ends switch
+}
+			else if (ch == 	'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9') {
+				output = output*10 + ch;
+}		
+
+}//ends the while loop
+}//ends the function definition
