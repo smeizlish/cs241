7b6b4df2d94775148bebeaffd7b4499972fdb15e
zackalak <zsheldon@oberlin.edu>
2015-10-14 21:15:25 -0400

    Added exp function

--- getnum.h
+++ getnum.h
@@ -1,4 +1,4 @@
-#getnum.h
+//getnum.h
 #include <limits.h>
 #include <stdio.h>
 #include <ctype.h>
@@ -9,3 +9,16 @@
 long getnum(void);
 
 #endif
+
+//first input value is the base
+//second input value is the number it is raised to
+//ex: e^x
+
+long exp(int e, int x){
+  int i;
+  long ret=1;
+  for (i=0; i < x; i++; ){
+    ret = ret*e;
+  }
+  return ret;
+}
