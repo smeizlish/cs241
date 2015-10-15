66b21f73f5edd6fa9d299536003f7037f391bcde
Sam Meizlish <smeizlis@clyde>
2015-10-15 19:01:52 -0400

    removed commented code

--- todecimal.c
+++ todecimal.c
@@ -8,21 +8,3 @@ int main( void )
     printf( "%ld\n", num );
 } 
 
-//getnum() handles all this??
-//  while ((ch = getchar()) != EOF){
-//    ungetc(ch, stdin);
-//    num = getnum();
-//    printf("%ld\n", num);
-//    ch = getchar();
-//    if (ch == EOF){
-//      break;
-//    }
-//    else if (isspace(ch)){
-//      ch = getchar();
-//      while (isspace(ch)){
-//        ch = getchar();
-//      }
-//      ungetc(ch, stdin);
-//    }
-//  }
-//}
