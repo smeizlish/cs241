63f116af5a0357a643a6df990acd28e37e2e5d70
zackalak <zsheldon@oberlin.edu>
2015-10-14 23:26:59 -0400

    added functionality to handle multiple input
    Does not handle errors or bad input yet. Has not been tested.

--- to decimal.c
+++ to decimal.c
@@ -3,6 +3,20 @@
 long num;
 
 void main(){
-  num = getnum();
-  printf("%ld", num);
+  while ((ch = getchar()) != EOF){
+    ungetc(ch, stdin);
+    num = getnum();
+    printf("%ld\n", num);
+    ch = getchar();
+    if (ch == EOF){
+      break;
+    }
+    else if (isspace(ch)){
+      ch = getchar();
+      while (isspace(ch)){
+        ch = getchar();
+      }
+      ungetc(ch, stdin);
+    }
+  }
 }
