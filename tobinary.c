311cdceb26e029e9e0a3ad35020debcb4abb26dd
zackalak <zsheldon@oberlin.edu>
2015-10-14 23:16:08 -0400

    Update tobinary.c

--- tobinary.c
+++ tobinary.c
@@ -1,8 +1,20 @@
 #include "getnum.c"
 
 long num;
+long converted;
+
+long bin(long x){
+  int i = sizeof(x)*CHAR_BIT;
+  
+  while (i > 0) {
+        --i;
+        converted *= 10;
+        converted |=  (x >>i)&1;
+  }
+  return x;
+}
 
 void main(){
-  num = getnum();
+  num = bin(getnum());
   printf("0b%ld", num);
 }
