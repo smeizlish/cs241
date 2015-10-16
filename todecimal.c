50a734e044e368c6de933481426d4209b9f226dc
Sam Meizlish <smeizlis@clyde>
2015-10-16 17:14:54 -0400

    more

--- todecimal.c
+++ todecimal.c
@@ -6,8 +6,10 @@ int ch;
 int main( void ){
     while (ch != EOF)
     {
-	if ( !isspace( ch ) ) ungetc( ch, stdin );
-	num = getnum();
+	if ( !isspace( ch ) ) {
+	    ungetc( ch, stdin );
+	    num = getnum();
+	}
 	if (neg=='1'){
 	    printf("-%ld\n", num);
 	}
