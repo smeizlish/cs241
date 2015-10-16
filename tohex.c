8d48ac9ede7b351e0ce48f5a811149a08cb9240e
Sam Meizlish <smeizlis@clyde>
2015-10-16 00:19:01 -0400

    clean tohex

--- tohex.c
+++ tohex.c
@@ -4,27 +4,15 @@ signed long num;
 int ch;
 
 int main( void ){
-    /*extern char negative;
-    num = getnum();
-    if (neg == '1'){
-	printf("-0x%lx\n", num);
-    }
-    else{
-	printf("0x%lx\n", num);
-    }
-    ch = getchar();
-    */
     while (ch != EOF)
     {
 	num = getnum();
-	//if statement for negative flag??
 	if (neg=='1'){
 	    printf("-0x%lx\n", num);
 	}
 	else{
 	    printf("0x%lx\n", num);
 	}
-
     }
 }
 
