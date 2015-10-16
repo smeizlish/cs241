ed7c756052825ede5ec42cf8af0865727f633347
zackalak <zsheldon@oberlin.edu>
2015-10-15 22:57:05 -0400

    Update todecimal.c

--- todecimal.c
+++ todecimal.c
@@ -1,10 +1,37 @@
-#include "getnum.c"
+#include "getnum.h"
 
-long num;
+signed long num;
+int ch;
 
-int main( void )
-{    
+int main( void ){
+    extern char negative;
     num = getnum();
-    printf( "%ld\n", num );
-} 
+    if (neg == '1'){
+	printf("-%ld\n", num);
+    }
+    else{
+	printf("%ld\n", num);
+    }
+    ch = getchar();
+    while (ch != EOF){
+	if (isspace(ch){
+	    while (isspace(ch){
+		ch = getchar();
+		printf("%d",ch);
+		    }
+		}
+	printf("%s", "test");
+	num = getnum();
+	//if statement for negative flag??
+	if (neg=='1'){
+	    printf("-%ld\n", num);
+	}
+	else{
+	    printf("%ld\n", num);
+	}
+
+    }
+}
+
+
 
